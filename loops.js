11b546b35c17c3ef4b500d37a478542ea8eded87
Robert Peck <rspeckpfs@gmail.com>
2017-06-26 07:03:52 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+var forLoop = function(array) {
+  for (var i = 0; i < 25; i ++) {
+    if(i === 1) {
+      array.push('I am 1 strange loop')
+    }
+    else {
+      array.push('I am ${i} strange loops')
+    }
+  }
+  return array
+}
+
+var whileLoop = function(number) {
+  while(number > 0) {
+    console.log(number)
+    number --
+  }
+  return 'done'
+}
+
+var doWhileLoop = function(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array.pop()
+  }
+  while(maybeTrue() && array.length >0)
+  return array
+}
